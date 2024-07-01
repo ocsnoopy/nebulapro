c41b207b6209651fec1f79f911b2619fcb1b4057
ocsnoopy <olti.cukali123@gmail.com>
2024-07-01 19:26:59 +0200

    some changes on on_delete

--- task_management/models.py
+++ task_management/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from user_management.models import User
 from datetime import datetime, date
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from user_management.models import User
 
 PROJECT_STATUS_CHOICES = (
     ('draft','Draft'),
@@ -29,12 +32,12 @@ class Industry(models.Model):
         verbose_name_plural = 'industries'
 
 class Client(models.Model):
-    industry = models.ForeignKey(Industry, null = True, on_delete=models.CASCADE)
+    industry = models.ForeignKey(Industry, null = True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=225)
     email = models.EmailField(null = True, max_length= 100)
     phone_number = models.CharField(max_length= 20, null = True)
     active = models.BooleanField(default= False)
-    user = models.ForeignKey(User, null = True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null = True, on_delete=models.SET_NULL)  
 
     def __str__(self):
         return self.name
@@ -55,11 +58,11 @@ class Task(models.Model):
     title = models.CharField(max_length=225)
     project = models.ForeignKey(Project, null = True, on_delete=models.CASCADE)
     status = models.CharField(max_length=225, choices=TASK_STATUS_CHOICES, default = 'back_log')
-    reporter = models.ForeignKey(User, related_name='reported_tasks', null = True, on_delete=models.CASCADE)
-    assignee = models.ForeignKey(User, related_name='assigned_tasks', null = True, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(User, related_name='reported_tasks', null = True, on_delete=models.SET_NULL)
+    assignee = models.ForeignKey(User, related_name='assigned_tasks', null = True, on_delete=models.SET_NULL)
     deadline_date = models.DateField()
     last_modified_date = models.DateTimeField()
-    last_modified_by = models.ForeignKey(User, related_name='modified_tasks', null = True, on_delete=models.CASCADE)
+    last_modified_by = models.ForeignKey(User, related_name='modified_tasks', null = True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
--- user_management/models.py
+++ user_management/models.py
@@ -11,5 +11,7 @@ class Role(models.Model):
     
 
 class User(AbstractUser):
-    role = models.ForeignKey(Role, null = True, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, null = True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.username
