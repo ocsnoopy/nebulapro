e9c031bf61420c26c6bf299a9b4bdb13c8e83790
ocsnoopy <olti.cukali123@gmail.com>
2024-07-02 15:21:54 +0200

    project list for clients

--- task_management/urls.py
+++ task_management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ClientListView
+from .views import ClientListView, ClientProjectsListView
 
 
 urlpatterns = [
     path('clients/', ClientListView.as_view(), name='client-list'),
+    path('clients/<int:pk>/projects/', ClientProjectsListView.as_view(), name='client_projects'),
 ]
--- task_management/views.py
+++ task_management/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Client
+from .models import Client, Project
 
 class ClientListView(ListView):
     model = Client
     template_name = 'clients/client_list.html'
+    context_object_name = 'clients'
+
+class ClientProjectsListView(ListView):
+    model = Project
+    template_name = 'clients/client_projects.html'
+    context_object_name = 'projects'
+
+
+    def get_queryset(self):
+        client_id = self.kwargs['pk']
+        client = get_object_or_404(Client, pk=client_id)
+        return Project.objects.filter(client=client)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        client_id = self.kwargs['pk']
+        client = get_object_or_404(Client, pk=client_id)
+        context['client'] = client
+        return context
