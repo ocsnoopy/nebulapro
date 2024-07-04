94ccc306dcb5dfbd1dd144d37ed586c34024b79c
ocsnoopy <olti.cukali123@gmail.com>
2024-07-04 18:42:34 +0200

    some color changes

--- nebulaproject/urls.py
+++ nebulaproject/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('task_management.urls')),
-    path('users/', include('user_management.urls')),
+    path('', include('user_management.urls')),
     path('', include('charges.urls')),
 ]
