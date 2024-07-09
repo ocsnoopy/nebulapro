7bf02d0fe0f05760df6b47d212da6f91f24d691a
ocsnoopy <olti.cukali123@gmail.com>
2024-07-09 19:08:02 +0200

    boostraped regisrration form

--- user_management/views.py
+++ user_management/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 from .models import Role, User
 from django.urls import reverse_lazy
+from .forms import SignUpForm
 
 class UsersListView(ListView):
     model = User
@@ -15,7 +16,7 @@ class RolesListView(ListView):
     context_object_name = 'roles'
 
 class UserRegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
